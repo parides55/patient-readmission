9dc8a3f42df5e3bd5fe910716a6938b66437252c
parides55 <andreasparides5@gmail.com>
2024-08-10 16:42:27 +0100

    correct a typo

--- app_pages/page_hypothesis.py
+++ app_pages/page_hypothesis.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-def page_hypothesis_bosy():
+def page_hypothesis_body():
     st.title("Hypothesis")
     st.write("This is the hypothesis page")
     st.write("This page will contain the hypothesis of the analysis and the results of the model")
--- app_pages/page_project_summary.py
+++ app_pages/page_project_summary.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-def page_summary_bosy():
+def page_summary_body():
     st.title("Summary")
     st.write("This is the summary page")
     st.write("This page will contain a summary of the analysis and the results of the model")
