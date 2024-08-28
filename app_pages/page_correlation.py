055cb189f36d447cbb5b6f7d7bfdb4281d70acb7
parides55 <andreasparides5@gmail.com>
2024-08-28 13:08:23 +0100

    add new most correlated features and use the new datasets for correlation, pps and parallel plot

--- app_pages/page_correlation.py
+++ app_pages/page_correlation.py
@@ -2,23 +2,19 @@ import streamlit as st
 import numpy as np
 import plotly.express as px
 import ppscore as pps
-from feature_engine.discretisation import ArbitraryDiscretiser
-from feature_engine.encoding import OrdinalEncoder
-from src.data_management import load_hospital_dataset, plot_categorical, plot_numerical, plot_parallel_plot
-
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+# functions from data_management.py to use 
+from src.data_management import load_hospital_dataset, plot_categorical
+from src.data_management import plot_numerical, plot_parallel_plot
 
 sns.set_style("darkgrid")
 
 
 def correlation(df, method):
     
-    ordinal = OrdinalEncoder(encoding_method='arbitrary',
-                            variables=df.columns[df.dtypes == 'object'].tolist())
-    df_encoded = ordinal.fit_transform(df)
-    
-    corr = df_encoded.corr(method=method)["readmitted"].sort_values(
+    corr = df.corr(method=method)["readmitted"].sort_values(
         ascending=False, key=abs)[1:].head(10)
     
     return corr
@@ -42,6 +38,7 @@ def heatmap_pps(df, threshold, figsize=(20, 12), font_annot=8):
 
 
 def plot_correlated_features(df, features):
+    
     for feature in features:
         if df[feature].dtype == "object":
             plot_categorical(df, feature)
@@ -51,17 +48,19 @@ def plot_correlated_features(df, features):
 
 def page_correlation_body():
     
-    #load data
-    df=load_hospital_dataset()
+    #datasets to be used
+    df = load_hospital_dataset("HospitalReadmissions.csv")
+    df_correlation = load_hospital_dataset("HospitalReadmissionsCorrelation.csv")
+    df_parallel = load_hospital_dataset("HospitalReadmissionsParallel.csv")
     
     #copied from the EDAandCorrelation notebook
-    correlated_features = ['diabetes_med',
-                    'n_emergency',
-                    'n_inpatient',
-                    'n_medications',
-                    'n_outpatient',
-                    'n_procedures'
-                    ]
+    correlated_features = ['A1Ctest',
+                            'glucose_test',
+                            'n_emergency',
+                            'n_inpatient',
+                            'n_lab_procedures',
+                            'n_outpatient'
+                        ]
     
     st.write(f"### **Feature Analysis**\n\n")
     
@@ -95,32 +94,39 @@ def page_correlation_body():
         f"#### **Correlation and PPS Study Summary**\n\n"
         f"* A correlation study was conducted in the notebook to better understand how "
         f"the feature variables are correlated and try to identify a pattern which will possibly "
-        f"help in the prediction of the target variable.\n"
-        f"After performing Spearman and Pearson correlation, the following variables "
+        f"help in the prediction of the target variable and to try to answer Business Requirement 1.\n"
+        f"* After performing Spearman and Pearson correlation, the following variables "
         f"are the most correlated: **{correlated_features}**\n"
-        f"* The correlation study revealed 'very weak' to 'weak' correlation between the "
-        f"feature variables and the target variable. \n"
+        f"* The correlation study, after applying the Log Transformer, Scaling and PCA, revealed "
+        f"moderate correlation between the feature variables and the target variable.\n"
         f"* Also, the PPS score was calculated to determine the predictive power of the "
-        f"variables, but it didn't show any predictive power for the target variable.\n"
+        f"variables,which show strong predictive power between the target variable and some features.\n"
     )
     
-    #checkboxes
-    if st.checkbox("View **Pearson** correlation results"):
-        st.write(correlation(df, method="pearson"))
+    #checkboxes for correlation and PPS
     if st.checkbox("View **Spearman** correlation results"):
-        st.write(correlation(df, method="spearman"))
+        st.write(correlation(df_correlation, method="spearman"))
+    if st.checkbox("View **Pearson** correlation results"):
+        st.write(correlation(df_correlation, method="pearson"))
     if st.checkbox("View PPS heatmap"):
-        st.write(heatmap_pps(df, 0.02))
+        st.write(heatmap_pps(df_correlation, 0.016))
     
     st.write("---")
     
     st.write(
         f"### **Most Correlated Features**\n\n"
         f"Below we are plotting the most correlated features with the target variable "
-        f"in a coountplot and also we are using a parallel plot to better visualize the "
-        f"pattern.\n"
+        f"in a countplot and also we are using a parallel plot to better visualize the "
+        f"pattern.\n\n"
+        f"Also, to avoid clutter and give better understanding of the features and their "
+        f"values, we have created the following bins for the Parallel Plot:\n"
+        f" * **n_emergency** - 'None', 'One','Up to 10','Above 10'\n"
+        f" * **n_inpatient** - 'None', 'One','Up to 5','Up to 10','Above 10'\n"
+        f" * **n_outpatient** - 'None', 'One','Up to 10','Up to 25'\n"
+        f" * **n_lab_procedures** - 'up to 10', '11-50', '51-100', 'above 100'\n"
     )
     
+    #checkboxes for most correlated features
     if st.checkbox("View Countplot"):
         plot_correlated_features(df, correlated_features)
     if st.checkbox("View Parallel Plot"):
@@ -129,8 +135,10 @@ def page_correlation_body():
     st.success(
         f"From the above correlation study and and by plotting the most correlated features "
         f"we notice a pattern for the readmitted patients.\n"
-        f"* A readmitted patient receives medicines for diabetes\n"
-        f"* A readmitted patient has 0 or very low number of emergency/inpatient/outpatient visits.\n"
-        f"* A readmitted patient receives between 11 and 20 medicines.\n"
-        f"* A readmitted patient has 0 or very low number of procedures."
+        f"* A readmitted patient typically has no A1Ctest.\n"
+        f"* A readmitted patient typically has no glucose test.\n"
+        f"* A readmitted patient typically has no emergency visit.\n"
+        f"* A readmitted patient typically has no outpatient visit.\n"
+        f"* A readmitted patient typically has between no inpatient visits and 5 inpatient visits.\n"
+        f"* A readmitted patient typically has high number of lab procedures.\n"
     )
