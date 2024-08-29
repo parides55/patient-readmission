a9f3e0890a9ace311344a10c0aeecf0a3ef59d6e
parides55 <andreasparides5@gmail.com>
2024-08-29 13:18:41 +0100

    add basic structure to the predict readmissions page

--- app_pages/page_predict_readmission.py
+++ app_pages/page_predict_readmission.py
@@ -1,4 +1,27 @@
 import streamlit as st
+import pandas as pd
+
+from src.data_management import load_hospital_dataset, load_pkl_file
+#from src.machine_learning.classification import predict_patient_readmission
 
 def page_prediction_body():
-    st.write(f"**Patient Readmission Prediction**")
+    
+    st.info(
+        f"##### Business requirement 2: Classification Model\n\n"
+        f" - We need to predict whether a patient will be readmitted or not.\n"
+        f" - For this task we need to build a binary classification model.\n"
+        f" - This will be performed during the Modeling and Evaluation step of "
+        f"the CRISP -M workflow.\n"
+    )
+    
+    X_live = DrawInputWidgets()
+    
+    # if st.button("Run Predictive Analysis"):
+    #     predict_patient_readmission(
+    #         X_live, dc_fe_pipeline, model_pipeline
+    #     )
+
+
+def DrawInputWidgets():
+    
+    df= load_hospital_dataset("HospitalReadmissions.csv")
