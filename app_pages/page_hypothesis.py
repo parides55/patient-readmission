42b1add1957429281786b085b3b8c1d79364c1eb
parides55 <andreasparides5@gmail.com>
2024-08-30 11:04:21 +0300

    make indentation corrections and adjust code for streamlit dashboard

--- app_pages/page_hypothesis.py
+++ app_pages/page_hypothesis.py
@@ -9,79 +9,90 @@ from src.data_management import load_hospital_dataset
 
 
 def hypothesis_1():
-  
-  df_parallel[['n_lab_procedures_bins','readmitted']].value_counts(normalize=True).mul(100).round(2).astype(str)
-  
-  fig,ax = plt.subplots(figsize=(12,6))enumerate()
-  ax = sns.countplot(df_parallel, x=df_parallel['n_lab_procedures_bins'], hue=df_parallel['readmitted'])
-  st.pyplot(fig)
-  
-  # Hypothesis 1: Correlation between number of lab procedures and readmission
-# Pearson correlation test between n_lab_procedures and readmitted
-correlation, p_value_correlation = pearsonr(df['n_lab_procedures'], df['readmitted'])
+    
+    df_parallel = load_hospital_dataset("HospitalReadmissionsParallel.csv")
+    df = load_hospital_dataset("HospitalReadmissions.csv")
 
-# Hypothesis 1: T-test to compare average lab procedures between readmitted and non-readmitted patients
-readmitted_lab_procedures = df[df['readmitted'] == 1]['n_lab_procedures']
-non_readmitted_lab_procedures = df[df['readmitted'] == 0]['n_lab_procedures']
+    st.dataframe(
+        df_parallel[['n_lab_procedures_bins','readmitted']]
+        .value_counts(normalize=True).mul(100).round(2)
+        )
 
-t_stat_lab_procedures, p_value_lab_procedures = ttest_ind(readmitted_lab_procedures, non_readmitted_lab_procedures)
+    fig, ax = plt.subplots(figsize=(8,4))
+    ax = sns.countplot(data=df_parallel, x=df_parallel['n_lab_procedures_bins'], hue=df_parallel['readmitted'])
+    st.pyplot(fig)
 
-# Results
+    # Hypothesis 1: Correlation between number of lab procedures and readmission
+    # Pearson correlation test between n_lab_procedures and readmitted
+    correlation, p_value_correlation = pearsonr(df['n_lab_procedures'], df['readmitted'])
 
-print(f"Hypothesis 1 - Correlation: {correlation}, {p_value_correlation}")
-print(f"Hypothesis 1 - T-test: {t_stat_lab_procedures}, {p_value_lab_procedures}")
+    # Hypothesis 1: T-test to compare average lab procedures between readmitted and non-readmitted patients
+    readmitted_lab_procedures = df[df['readmitted'] == 1]['n_lab_procedures']
+    non_readmitted_lab_procedures = df[df['readmitted'] == 0]['n_lab_procedures']
+
+    t_stat_lab_procedures, p_value_lab_procedures = ttest_ind(readmitted_lab_procedures, non_readmitted_lab_procedures)
+
+    # Results
+
+    st.write(f"Hypothesis 1 - Correlation: {correlation}, {p_value_correlation}")
+    st.write(f"Hypothesis 1 - T-test: {t_stat_lab_procedures}, {p_value_lab_procedures}")
 
 
 def hypothesis_2():
-  
-  df = load_hospital_dataset("HospitalReadmissions.csv")
-  
-  df[['change','readmitted', 'diabetes_med']].value_counts(normalize=True).mul(100).round(2).astype(str) + '%'
-  
-  fig,ax = plt.subplots(figsize=(12,6))
-  ax = sns.barplot(df, x='change', y='readmitted', hue='diabetes_med')
-  st.pyplot(fig)
-  
-  # Hypothesis 2: Chi-Square test between change in diabetes medication and readmission
-contingency_table = pd.crosstab(df['change'], df['readmitted'])
-chi2, p_value_chi2, _, _ = chi2_contingency(contingency_table)
-
-# Results
-print(f"Hypothesis 2 - Chi-Square Test: {chi2}, {p_value_chi2}")
-  
-
-def hypothesis_3():
-  
-  df = load_hospital_dataset("HospitalReadmissions.csv")
-  
-  df[['age']].value_counts(normalize=True).mul(100).round(2).astype(str) + '%'
-  
-  fig,ax = plt.subplots(figsize=(12,6))
-    ax = sns.countplot(df, x=df['age'], hue=df_parallel['readmitted'])
+    
+    df = load_hospital_dataset("HospitalReadmissions.csv")
+    
+    st.dataframe(
+        df[['change','readmitted', 'diabetes_med']]
+    .value_counts(normalize=True).mul(100).round(2)
+    )
+    
+    fig,ax = plt.subplots(figsize=(8,4))
+    ax = sns.barplot(data=df, x='change', y='readmitted', hue='diabetes_med')
     st.pyplot(fig)
     
-  def categorize_age(age):
-    if age in ['[0-10)', '[10-20)', '[20-30)', '[30-40)', '[40-50)', '[50-60)']:
-        return 'Under 60'
-    elif age in ['[60-70)', '[70-80)']:
-        return '60-80'
-    elif age in ['[80-90)', '[90-100)']:
-        return 'Over 80'
-    else:
-        return 'Unknown'  # For any unexpected values
+    # Hypothesis 2: Chi-Square test between change in diabetes medication and readmission
+    contingency_table = pd.crosstab(df['change'], df['readmitted'])
+    chi2, p_value_chi2, _, _ = chi2_contingency(contingency_table)
 
-  # Apply the function to create a new column with categorized age groups
-  df_age_group = df.filter('age')
-  df_age_group['age_group'] = df['age'].apply(categorize_age)
-  
-  df_age_group['age_group'].value_counts(normalize=True).mul(100).round(2).astype(str) + '%'
-  
-  contingency_table_age = pd.crosstab(df_age_group['age_group'], df['readmitted'])
-  chi2_age, p_value_age, _, _ = chi2_contingency(contingency_table_age)
-  
-  # Results
-  print(f"Chi-Square Statistic: {chi2_age}")
-  print(f"P-Value: {p_value_age}")
+    # Results
+    st.write(f"Hypothesis 2 - Chi-Square Test: {chi2}, {p_value_chi2}")
+    
+
+def hypothesis_3():
+    
+    df = load_hospital_dataset("HospitalReadmissions.csv")
+    
+    st.dataframe(
+        df[['age']].value_counts(normalize=True).mul(100).round(2)
+        )
+    
+    fig,ax = plt.subplots(figsize=(8,4))
+    ax = sns.countplot(data=df, x=df['age'], hue=df['readmitted'])
+    st.pyplot(fig)
+        
+    def categorize_age(age):
+        if age in ['[0-10)', '[10-20)', '[20-30)', '[30-40)', '[40-50)', '[50-60)']:
+            return 'Under 60'
+        elif age in ['[60-70)', '[70-80)']:
+            return '60-80'
+        elif age in ['[80-90)', '[90-100)']:
+            return 'Over 80'
+        else:
+            return 'Unknown'  # For any unexpected values
+
+    # Apply the function to create a new column with categorized age groups
+    df_age_group = df[['age']].copy()
+    df_age_group['age_group'] = df['age'].apply(categorize_age)
+    
+    df_age_group['age_group'].value_counts(normalize=True).mul(100).round(2).astype(str) + '%'
+    
+    contingency_table_age = pd.crosstab(df_age_group['age_group'], df['readmitted'])
+    chi2_age, p_value_age, _, _ = chi2_contingency(contingency_table_age)
+    
+    # Results
+    st.write(f"Chi-Square Statistic: {chi2_age}")
+    st.write(f"P-Value: {p_value_age}")
 
 
 def page_hypothesis_body():
@@ -97,13 +108,12 @@ def page_hypothesis_body():
     st.write(f"##### Findings:\n\n")
     
     st.warning(
-        f"* There is a very weak positive correlation between the number of lab procedures and readmission, but it is statistically significant due to the low p-value.\n"
-        f"There is a very weak positive correlation between the number of lab procedures and readmission, but it is statistically significant due to the low p-value.\n\n"
+        f"* There is a very weak positive correlation between the number of lab procedures and readmission, but it is statistically significant due to the low p-value.\n\n"
         f"**Conclusion**: Hypothesis 1 is supported by the data, as there is a statistically significant but weak relationship between the number of lab procedures and the likelihood of readmission."
     )
     
-    if st.checkbox():
-      hypothesis_1()
+    if st.checkbox("Check the data for hypothesis 1"):
+        hypothesis_1()
     
     st.info(
         f"###### **Hypothesis 2**\n"
@@ -120,8 +130,8 @@ def page_hypothesis_body():
             f"**Conclusion**: Hypothesis 2 is strongly supported by the data, indicating that patients who had a change in their diabetes medication are indeed more likely to be readmitted."
         )
         
-        if st.checkbox():
-          hypothesis_2()
+    if st.checkbox("Check the data for hypothesis 2"):
+        hypothesis_2()
     
     st.info(
         f"###### **Hypothesis 3**\n"
@@ -137,5 +147,5 @@ def page_hypothesis_body():
                 f"**Conclusion**: Hypothesis 3 is valid based on the data, indicating that higher age groups have a higher probability of readmission."
             )
             
-            if st.checkbox():
-              hypothesis_3()
+    if st.checkbox("Check the data for hypothesis 3"):
+        hypothesis_3()
