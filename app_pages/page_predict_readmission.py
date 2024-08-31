0947435ab4be1f1be86b5d9374b23ad42bdc6916
parides55 <andreasparides5@gmail.com>
2024-08-31 16:56:31 +0300

    remove an input widget and rearrange the layout

--- app_pages/page_predict_readmission.py
+++ app_pages/page_predict_readmission.py
@@ -2,24 +2,24 @@ import streamlit as st
 import pandas as pd
 
 from src.data_management import load_hospital_dataset, load_pkl_file
-#from src.machine_learning.classification import predict_patient_readmission
+#from src.machine_learning.predict_readmission import predict_patient_readmission
 
 def page_prediction_body():
     
     version = "v1"
     # dc_fe_pipeline = load_pkl_file(
-    #     f"outputs/ml_pipeline/classification_model/{version}/data_cleaning_and_feat_engineering_pipeline.pkl"
+    #     f"outputs/ml_pipeline/predict_readmission/{version}/data_cleaning_and_feat_engineering_pipeline.pkl"
     # )
     # model_pipeline = load_pkl_file(
-    #     f"outputs/ml_pipeline/classification_model/{version}/classification_pipeline.pkl"
+    #     f"outputs/ml_pipeline/predict_readmission/{version}/classification_pipeline.pkl"
     # )
     
     st.info(
         f"##### Business requirement 2: Classification Model\n\n"
         f" - We need to predict whether a patient will be readmitted or not.\n"
         f" - For this task we need to build a binary classification model.\n"
-        f" - This will be performed during the Modeling and Evaluation step of "
-        f"the CRISP -M workflow.\n"
+        f" - Extensive hyperparameter optimization will give us the best "
+        f"chance at a highly accurate prediction.\n"
     )
     
     st.write("---")
@@ -35,13 +35,11 @@ def DrawInputsWidgets():
     
         # load dataset
         df = load_hospital_dataset('HospitalReadmissions.csv')
-        percentageMin, percentageMax = 0.4, 2.0
     
     # we create input widgets only for 6 features
-        col1, col2, col3, col4 = st.columns(4)
-        col5, col6, col7, col8 = st.columns(4)
-        col9, col10, col11, col12 = st.columns(4)
-        col13, col14, col15, col16 = st.columns(4)
+        col1, col2, col3, col4, col5 = st.columns(5)
+        col6, col7, col8, col9, col10 = st.columns(5)
+        col11, col12, col13, col14, col15 = st.columns(5)
     
         # create an empty DataFrame, which will be the live data
         X_live = pd.DataFrame([], index=[0])
@@ -120,14 +118,6 @@ def DrawInputsWidgets():
         X_live[feature] = st_widget
         
         with col9:
-            feature = "medical_specialty"
-            st_widget = st.selectbox(
-                label=feature,
-                options=df[feature].unique()
-            )
-        X_live[feature] = st_widget
-        
-        with col10:
             feature = "diag_1"
             st_widget = st.selectbox(
                 label=feature,
@@ -135,7 +125,7 @@ def DrawInputsWidgets():
             )
         X_live[feature] = st_widget
         
-        with col11:
+        with col10:
             feature = "diag_2"
             st_widget = st.selectbox(
                 label=feature,
@@ -143,7 +133,7 @@ def DrawInputsWidgets():
             )
         X_live[feature] = st_widget
         
-        with col12:
+        with col11:
             feature = "diag_3"
             st_widget = st.selectbox(
                 label=feature,
@@ -151,7 +141,7 @@ def DrawInputsWidgets():
             )
         X_live[feature] = st_widget
         
-        with col13:
+        with col12:
             feature = "glucose_test"
             st_widget = st.selectbox(
                 label=feature,
@@ -159,7 +149,7 @@ def DrawInputsWidgets():
             )
         X_live[feature] = st_widget
         
-        with col14:
+        with col13:
             feature = "A1Ctest"
             st_widget = st.selectbox(
                 label=feature,
@@ -167,7 +157,7 @@ def DrawInputsWidgets():
             )
         X_live[feature] = st_widget
         
-        with col15:
+        with col14:
             feature = "change"
             st_widget = st.selectbox(
                 label=feature,
@@ -175,14 +165,12 @@ def DrawInputsWidgets():
             )
         X_live[feature] = st_widget
         
-        with col16:
+        with col15:
             feature = "diabetes_med"
             st_widget = st.selectbox(
                 label=feature,
                 options=df[feature].unique()
             )
         X_live[feature] = st_widget
-    
-        # # st.write(X_live)
     
         return X_live
