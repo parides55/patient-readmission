e7bb077f7ae9660fb18099431281a962bb4c7f9e
parides55 <andreasparides5@gmail.com>
2024-09-03 03:29:48 +0300

    fix indentation to meet PEP8 requirements

--- app_pages/page_predict_readmission.py
+++ app_pages/page_predict_readmission.py
@@ -4,16 +4,19 @@ import pandas as pd
 from src.data_management import load_hospital_dataset, load_pkl_file
 from src.machine_learning.predict_readmission import predict_readmission
 
+
 def page_prediction_body():
-    
+
     version = "v1"
     dc_fe_pipeline = load_pkl_file(
-        f"outputs/ml_pipeline/predict_readmission/{version}/clf_pipeline_data_cleaning_feat_eng.pkl"
+        f"outputs/ml_pipeline/predict_readmission/{version}/"
+        f"clf_pipeline_data_cleaning_feat_eng.pkl"
     )
     model_pipeline = load_pkl_file(
-        f"outputs/ml_pipeline/predict_readmission/{version}/clf_pipeline_model.pkl"
+        f"outputs/ml_pipeline/predict_readmission/{version}/"
+        f"clf_pipeline_model.pkl"
     )
-    
+
     st.info(
         f"##### Business requirement 2: Classification Model\n\n"
         f" - We need to predict whether a patient will be readmitted or not.\n"
@@ -21,11 +24,11 @@ def page_prediction_body():
         f" - Extensive hyperparameter optimization will give us the best "
         f"chance at a highly accurate prediction.\n"
     )
-    
+
     st.write("---")
-    
+
     X_live = DrawInputsWidgets()
-    
+
     if st.button("Run Predictive Analysis"):
         st.write(
             predict_readmission(X_live, dc_fe_pipeline, model_pipeline)
@@ -33,145 +36,146 @@ def page_prediction_body():
 
 
 def DrawInputsWidgets():
-    
-        # load dataset
-        df = load_hospital_dataset('HospitalReadmissions.csv')
-    
+
+    # load dataset
+    df = load_hospital_dataset('HospitalReadmissions.csv')
+
     # we create input widgets only for 6 features
-        col1, col2, col3, col4, col5 = st.columns(5)
-        col6, col7, col8, col9, col10 = st.columns(5)
-        col11, col12, col13, col14, col15 = st.columns(5)
-    
-        # create an empty DataFrame, which will be the live data
-        X_live = pd.DataFrame([], index=[0])
-    
-        # from here on we draw the widget based on the variable type (numerical or categorical)
-        # and set initial values
-        with col1:
-            feature = "age"
-            st_widget = st.selectbox(
-                label=feature,
-                options=df[feature].unique()
-            )
-        X_live[feature] = st_widget
-    
-        with col2:
-            feature = "time_in_hospital"
-            st_widget = st.number_input(
+    col1, col2, col3, col4, col5 = st.columns(5)
+    col6, col7, col8, col9, col10 = st.columns(5)
+    col11, col12, col13, col14, col15 = st.columns(5)
+
+    # create an empty DataFrame, which will be the live data
+    X_live = pd.DataFrame([], index=[0])
+
+    # from here on we draw the widget based on the variable type
+    # (numerical or categorical)
+    # and set initial values
+    with col1:
+        feature = "age"
+        st_widget = st.selectbox(
+            label=feature,
+            options=df[feature].unique()
+        )
+    X_live[feature] = st_widget
+
+    with col2:
+        feature = "time_in_hospital"
+        st_widget = st.number_input(
             label=feature,
             min_value=0,
             value=int(df[feature].median())
             )
-        X_live[feature] = st_widget
-    
-        with col3:
-            feature = "n_lab_procedures"
-            st_widget = st.number_input(
+    X_live[feature] = st_widget
+
+    with col3:
+        feature = "n_lab_procedures"
+        st_widget = st.number_input(
             label=feature,
             min_value=0,
             value=int(df[feature].median())
             )
-        X_live[feature] = st_widget
-    
-        with col4:
-            feature = "n_procedures"
-            st_widget = st.number_input(
+    X_live[feature] = st_widget
+
+    with col4:
+        feature = "n_procedures"
+        st_widget = st.number_input(
             label=feature,
             min_value=0,
             value=int(df[feature].median())
             )
-        X_live[feature] = st_widget
-    
-        with col5:
-            feature = "n_medications"
-            st_widget = st.number_input(
+    X_live[feature] = st_widget
+
+    with col5:
+        feature = "n_medications"
+        st_widget = st.number_input(
             label=feature,
             min_value=0,
             value=int(df[feature].median())
             )
-        X_live[feature] = st_widget
-    
-        with col6:
-            feature = "n_outpatient"
-            st_widget = st.number_input(
+    X_live[feature] = st_widget
+
+    with col6:
+        feature = "n_outpatient"
+        st_widget = st.number_input(
             label=feature,
             min_value=0,
             value=int(df[feature].median())
             )
-        X_live[feature] = st_widget
-        
-        with col7:
-            feature = "n_inpatient"
-            st_widget = st.number_input(
+    X_live[feature] = st_widget
+
+    with col7:
+        feature = "n_inpatient"
+        st_widget = st.number_input(
             label=feature,
             min_value=0,
             value=int(df[feature].median())
             )
-        X_live[feature] = st_widget
-        
-        with col8:
-            feature = "n_emergency"
-            st_widget = st.number_input(
+    X_live[feature] = st_widget
+
+    with col8:
+        feature = "n_emergency"
+        st_widget = st.number_input(
             label=feature,
             min_value=0,
             value=int(df[feature].median())
             )
-        X_live[feature] = st_widget
-        
-        with col9:
-            feature = "diag_1"
-            st_widget = st.selectbox(
-                label=feature,
-                options=df[feature].unique()
-            )
-        X_live[feature] = st_widget
-        
-        with col10:
-            feature = "diag_2"
-            st_widget = st.selectbox(
-                label=feature,
-                options=df[feature].unique()
-            )
-        X_live[feature] = st_widget
-        
-        with col11:
-            feature = "diag_3"
-            st_widget = st.selectbox(
-                label=feature,
-                options=df[feature].unique()
-            )
-        X_live[feature] = st_widget
-        
-        with col12:
-            feature = "glucose_test"
-            st_widget = st.selectbox(
-                label=feature,
-                options=df[feature].unique()
-            )
-        X_live[feature] = st_widget
-        
-        with col13:
-            feature = "A1Ctest"
-            st_widget = st.selectbox(
-                label=feature,
-                options=df[feature].unique()
-            )
-        X_live[feature] = st_widget
-        
-        with col14:
-            feature = "change"
-            st_widget = st.selectbox(
-                label=feature,
-                options=df[feature].unique()
-            )
-        X_live[feature] = st_widget
-        
-        with col15:
-            feature = "diabetes_med"
-            st_widget = st.selectbox(
-                label=feature,
-                options=df[feature].unique()
-            )
-        X_live[feature] = st_widget
-    
-        return X_live
+    X_live[feature] = st_widget
+
+    with col9:
+        feature = "diag_1"
+        st_widget = st.selectbox(
+            label=feature,
+            options=df[feature].unique()
+        )
+    X_live[feature] = st_widget
+
+    with col10:
+        feature = "diag_2"
+        st_widget = st.selectbox(
+            label=feature,
+            options=df[feature].unique()
+        )
+    X_live[feature] = st_widget
+
+    with col11:
+        feature = "diag_3"
+        st_widget = st.selectbox(
+            label=feature,
+            options=df[feature].unique()
+        )
+    X_live[feature] = st_widget
+
+    with col12:
+        feature = "glucose_test"
+        st_widget = st.selectbox(
+            label=feature,
+            options=df[feature].unique()
+        )
+    X_live[feature] = st_widget
+
+    with col13:
+        feature = "A1Ctest"
+        st_widget = st.selectbox(
+            label=feature,
+            options=df[feature].unique()
+        )
+    X_live[feature] = st_widget
+
+    with col14:
+        feature = "change"
+        st_widget = st.selectbox(
+            label=feature,
+            options=df[feature].unique()
+        )
+    X_live[feature] = st_widget
+
+    with col15:
+        feature = "diabetes_med"
+        st_widget = st.selectbox(
+            label=feature,
+            options=df[feature].unique()
+        )
+    X_live[feature] = st_widget
+
+    return X_live
--- app_pages/page_project_summary.py
+++ app_pages/page_project_summary.py
@@ -1,74 +1,95 @@
 import streamlit as st
 import pandas as pd
 
-DATASET = pd.read_csv(f"outputs/datasets/collection/HospitalReadmissions.csv").head(10)
+DATASET = pd.read_csv(
+    f"outputs/datasets/collection/HospitalReadmissions.csv").head(10)
+
 
 def page_summary_body():
     st.write(f'#### **Project Summary**\n\n')
-    
+
     st.write(
-        f'Generally the hospital readmissions are costly for Hospitals and Health providers '
-        f'and use a lot of resources that can be utilised better to achieve better and more '
-        f'expeditious health care services.\n\n'
-        f'The goal of this project is to develop a predictive analytics application that'
-        f'forecasts the likelihood of patient readmission after discharge from the hospital. '
-        f'This application aims to assist healthcare providers in identifying high-risk patients, '
-        f'thereby enabling targeted interventions to reduce readmission rates and '
-        f'improve patient outcomes.'
+        f'Generally the hospital readmissions are costly for Hospitals and '
+        f'Health providers and use a lot of resources that can be utilised '
+        f'better to achieve better and more expeditious health care '
+        f'services.\n\n'
+        f'The goal of this project is to develop a predictive analytics '
+        f'application that forecasts the likelihood of patient readmission '
+        f'after discharge from the hospital.'
+        f'This application aims to assist healthcare providers in identifying '
+        f'high-risk patients, thereby enabling targeted interventions to '
+        f'reduce readmission rates and improve patient outcomes.'
     )
-    
+
     st.write(
-        f"* Visit the [project's documentation](https://github.com/parides55/patient-readmission) "
-        f"for more information.\n"
+        f"* Visit the [project's documentation](https://github.com/"
+        f"parides55/patient-readmission) for more information.\n"
     )
-    
+
     st.info(
         f"#### **Project's Dataset**\n\n"
-        f"The dataset used in this project, sourced from Kaggle, contains 25,000 records of hospital "
-        f"admissions, with 17 attributes describing various aspects of patient demographics, "
-        f"clinical factors, and hospital stay details. Key features include: \n"
+        f"The dataset used in this project, sourced from Kaggle, contains "
+        f"25,000 records of hospital admissions, with 17 attributes "
+        f"describing various aspects of patient demographics, clinical "
+        f"factors, and hospital stay details. Key features include: \n"
         f"* **Patient Demographics:** Age group.\n"
-        f"* Hospital Stay Information: Length of stay (time_in_hospital), number of procedures, "
-        f"medications, outpatient visits, inpatient visits, and emergency visits.\n"
-        f"* Clinical Information: Primary, secondary, and tertiary diagnoses (diag_1, diag_2, diag_3), "
-        f"glucose levels, and A1C test results."
-        f"*(The A1C test is a common blood test used to diagnose type 1 and type 2 diabetes. "
-        f"If you're living with diabetes, the test is also used to monitor how well you're "
-        f"managing blood sugar levels.)*\n"
-        f"* Treatment Information: Whether the patient's treatment plan included changes (change) "
-        f"in their medication, and whether diabetes medications were administered (diabetes_med).\n"
-        f"* Target Variable: A binary indicator (readmitted) showing whether the patient was readmitted.\n"
+        f"* Hospital Stay Information: Length of stay (time_in_hospital), "
+        f"number of procedures, medications, outpatient visits, "
+        f"inpatient visits, and emergency visits.\n"
+        f"* Clinical Information: Primary, secondary, and tertiary diagnoses "
+        f"(diag_1, diag_2, diag_3), glucose levels, and A1C test results."
+        f"*(The A1C test is a common blood test used to diagnose type 1 and "
+        f"type 2 diabetes. if you're living with diabetes, the test is also "
+        f"used to monitor how well you're managing blood sugar levels.)*\n"
+        f"* Treatment Information: Whether the patient's treatment plan "
+        f"included changes (change) in their medication, and whether diabetes "
+        f"medications were administered (diabetes_med).\n"
+        f"* Target Variable: A binary indicator (readmitted) showing whether "
+        f"the patient was readmitted.\n"
     )
-    
+
     st.write(f"Below find the first 10 rows of the dataset.\n")
-    
+
     st.dataframe(DATASET)
-    
+
     st.info(
         f"#### **Feature Terminology**\n\n"
         f"* **age** - age bracket of the patient\n"
         f"* **time_in_hospital** - days (from 1 to 14)\n"
-        f"* **n_procedures** - number of procedures performed during the hospital stay\n"
-        f"* **n_lab_procedures** - number of laboratory procedures performed during the hospital stay\n"
-        f"* **n_medications** - number of medications administered during the hospital stay\n"
-        f"* **n_outpatient** - number of outpatient visits in the year before a hospital stay\n"
-        f"* **n_inpatient** - number of inpatient visits in the year before the hospital stay\n"
-        f"* **n_emergency** - number of visits to the emergency room in the year before the hospital stay\n"
+        f"* **n_procedures** - number of procedures performed during the "
+        f"hospital stay\n"
+        f"* **n_lab_procedures** - number of laboratory procedures performed "
+        f"during the hospital stay\n"
+        f"* **n_medications** - number of medications administered during the "
+        f"hospital stay\n"
+        f"* **n_outpatient** - number of outpatient visits in the year before "
+        f"a hospital stay\n"
+        f"* **n_inpatient** - number of inpatient visits in the year before "
+        f"the hospital stay\n"
+        f"* **n_emergency** - number of visits to the emergency room in the "
+        f"year before the hospital stay\n"
         f"* **medical_specialty** - the specialty of the admitting physician\n"
-        f"* **diag_1** - primary diagnosis (Circulatory, Respiratory, Digestive, etc.)\n"
+        f"* **diag_1** - primary diagnosis (Circulatory, Respiratory, "
+        f"Digestive, etc.)\n"
         f"* **diag_2** - secondary diagnosis\n"
         f"* **diag_3** - additional secondary diagnosis\n"
-        f"* **glucose_test** - whether the glucose serum came out as high (> 200), normal, or not performed\n"
-        f"* **A1Ctest** - whether the A1C level of the patient came out as high (> 7%), normal, or not performed\n"
-        f"* **change** - whether there was a change in the diabetes medication (1 = 'yes' or 0 = 'no')\n"
-        f"* **diabetes_med** - whether a diabetes medication was prescribed (1 = 'yes' or 0 = 'no')\n"
-        f"* **readmitted** - if the patient was readmitted at the hospital (1 = 'yes' or 0 = 'no')\n"
+        f"* **glucose_test** - whether the glucose serum came out as high "
+        f"(> 200), normal, or not performed\n"
+        f"* **A1Ctest** - whether the A1C level of the patient came out as "
+        f"high (> 7%), normal, or not performed\n"
+        f"* **change** - whether there was a change in the diabetes "
+        f"medication (1 = 'yes' or 0 = 'no')\n"
+        f"* **diabetes_med** - whether a diabetes medication was prescribed "
+        f"(1 = 'yes' or 0 = 'no')\n"
+        f"* **readmitted** - if the patient was readmitted at the hospital "
+        f"(1 = 'yes' or 0 = 'no')\n"
     )
-    
+
     st.success(
         f"#### **Business Requirements**\n\n"
-        f"**Business Requirements 1** - The client is interested to know the key factors "
-        f"that will make a patient to be readmitted.\n\n"
-        f"**Business Requirements 2** - The client is interested to know whether a patient "
-        f"will be readmitted or not by using patient's data.\n\n"
+        f"**Business Requirements 1** - The client is interested to know the "
+        f"key factors that will make a patient to be readmitted.\n\n"
+        f"**Business Requirements 2** - The client is interested to know "
+        f"whether a patient will be readmitted or not by using patient's "
+        f"data.\n\n"
     )
